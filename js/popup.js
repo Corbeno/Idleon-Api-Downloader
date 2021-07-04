b2a1ce72823a9a288b013bb417ab7219e8c1f65e
Corbeno <corbin@kemsfamily.us>
2021-07-04 13:45:20 -0500

    update formatting

--- js/popup.js
+++ js/popup.js
@@ -1,21 +1,22 @@
-console.log("popup");
 updateAllButtons();
-chrome.storage.onChanged.addListener(function(changes, namespace){
-    for (let [key, { oldValue, newValue }] of Object.entries(changes)) {
-        if(newValue != null){
-            //when save data changes, re-parse clean json with new save data and update buttons
+chrome.storage.onChanged.addListener(function (changes, namespace) {
+    for (let [key, {
+            oldValue,
+            newValue
+        }
+    ] of Object.entries(changes)) {
+        if (newValue != null) { // when save data changes, re-parse clean json with new save data and update buttons
             updateAllButtons();
         }
     }
 });
 
-function updateAllButtons(){
-    chrome.storage.local.get("data", function(result){
-        if(result.data != null){
-            //save data
+function updateAllButtons() {
+    chrome.storage.local.get("data", function (result) {
+        if (result.data != null) { // save data
             var rawJson = result.data;
             var rawString = JSON.stringify(rawJson);
-            var cleanJson =  parseData(rawJson);
+            var cleanJson = parseData(rawJson);
             var cleanString = JSON.stringify(cleanJson);
             var lootyString = rawJson.saveData.documentChange.document.fields.Cards1.stringValue.replace(/\"/g, "\\");
             var questsString = JSON.stringify(cleanJson.account.quests);
@@ -25,8 +26,8 @@ function updateAllButtons(){
             showDownloadButton("cleanJsonDownloadLink", cleanString, "cleanData.json");
             showCopyButton("lootyCopyLink", lootyString);
             showCopyButton("questsCopyLink", questsString);
-        
-            //companion data
+
+            // companion data
             var familyCsv = getFamilyCsv(cleanJson);
             var guildCsv = getGuildCsv(cleanJson);
             showCopyButton("familyCopyLink", familyCsv);
@@ -36,37 +37,39 @@ function updateAllButtons(){
     });
 }
 
-function showCharacterCopyButtons(cleanJson){
+function showCharacterCopyButtons(cleanJson) {
     var numChars = cleanJson.characters.length;
-            for(var i = 0; i < numChars; i++){
-                var charData = getCharacterCsv(cleanJson, i);
-                
-                var container = document.getElementById("char" + i + "CopyLink");
-                var a = document.createElement("a");
-                a.innerHTML = "char" + (i+1);
-                (function (_charData) {
-                    a.addEventListener("click", function(){
-                        copyTextToClipboard(_charData);
-                    });
-                })(charData);
-                a.addEventListener("click", function(charData){
-                    return function(){
-                        copyTextToClipboard(charData);
-                    }
-                }(a));
-                while (container.hasChildNodes()) {
-                    container.removeChild(container.lastChild);
-                }
-                container.appendChild(a);
+    for (var i = 0; i < numChars; i++) {
+        var charData = getCharacterCsv(cleanJson, i);
 
+        var container = document.getElementById("char" + i + "CopyLink");
+        var a = document.createElement("a");
+        a.innerHTML = "char" + (
+            i + 1
+        );
+        (function (_charData) {
+            a.addEventListener("click", function () {
+                copyTextToClipboard(_charData);
+            });
+        })(charData);
+        a.addEventListener("click", function (charData) {
+            return function () {
+                copyTextToClipboard(charData);
             }
+        }(a));
+        while (container.hasChildNodes()) {
+            container.removeChild(container.lastChild);
+        }
+        container.appendChild(a);
+
+    }
 }
 
-function showCopyButton(elementId, dataString){
+function showCopyButton(elementId, dataString) {
     var container = document.getElementById(elementId);
     var a = document.createElement("a");
     a.innerHTML = "copy";
-    a.addEventListener("click", function(){
+    a.addEventListener("click", function () {
         copyTextToClipboard(dataString);
     });
     while (container.hasChildNodes()) {
@@ -75,7 +78,7 @@ function showCopyButton(elementId, dataString){
     container.appendChild(a);
 }
 
-function showDownloadButton(elementId, dataString, fileName){
+function showDownloadButton(elementId, dataString, fileName) {
     var data = "text/json;charset=utf-8," + encodeURIComponent(dataString);
     var a = document.createElement("a");
     a.href = "data:" + data;
@@ -88,15 +91,15 @@ function showDownloadButton(elementId, dataString, fileName){
     container.appendChild(a);
 }
 
-function showGuildCopyButton(){
-    chrome.storage.local.get("data", function(result){
-        if(result.data != null){
+function showGuildCopyButton() {
+    chrome.storage.local.get("data", function (result) {
+        if (result.data != null) {
             var cleanJson = parseData(JSON.stringify(result));
             var familyData = getGuildCsv(cleanJson);
             var container = document.getElementById("guildCopyLink");
             var a = document.createElement("a");
             a.innerHTML = "copy";
-            a.addEventListener("click", function(){
+            a.addEventListener("click", function () {
                 copyTextToClipboard(familyData);
             });
             while (container.hasChildNodes()) {
@@ -115,5 +118,4 @@ function copyTextToClipboard(text) {
     document.execCommand('copy');
     copyFrom.blur();
     document.body.removeChild(copyFrom);
-  }
-
+}
