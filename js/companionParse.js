2514a5f42d88fe9e11911a585ddbbcc3d6ba1a16
Corbeno <corbin@kemsfamily.us>
2021-08-31 22:30:11 -0500

    more work on companion parsing. Not done with char though

--- js/companionParse.js
+++ js/companionParse.js
@@ -3,7 +3,7 @@ function companionParseData(cleanData) {
     var r = {};
     r.alchemy = parseCompanionAlchemy(cleanData);
     r.cards = parseCompanionCards(cleanData);
-    r.chars = [];
+    r.chars = parseCompanionionChars(cleanData);
     r.checklist = {};
     r.starSigns = {};
     r.version = "0.2.3"; // unsure if this should be hardcoded
@@ -43,4 +43,61 @@ function parseCompanionAlchemy(clean) {
     r.vials = alchemy.vialLevels;
 
     return r;
+}
+
+function parseCompanionionChars(clean) {
+    var r = [];
+    var chars = clean.characters;
+    for (var char of chars) {
+        console.log(JSON.stringify(char));
+        r.push({
+            "class": char.class,
+            "level": char.level,
+            "name": char.name,
+            "items": {}, //TODO
+            "skills": {
+                "Mining": char.skillLevels.mining,
+                "Smithing": char.skillLevels.smithing,
+                "Choppin'": char.skillLevels.chopping,
+                "Fishing": char.skillLevels.fishing,
+                "Alchemy": char.skillLevels.alchemy,
+                "Catching": char.skillLevels.catching,
+                "Trapping": char.skillLevels.trapping,
+                "Construction": char.skillLevels.construction,
+                "Worship": char.skillLevels.worship
+            },
+            "statues": {
+                "Power": 0,
+                "Speed": 0,
+                "Mining": 0,
+                "Feasty": 0,
+                "Health": 0,
+                "Kachow": 0,
+                "Lumberbob": 0,
+                "Thicc Skin": 0,
+                "Oceanman": 0,
+                "Ol Reliable": 0,
+                "Exp Book": 0,
+                "Anvil": 0,
+                "Cauldron": 0,
+                "Beholder": 0,
+                "Bullseye": 0,
+                "Box": 0,
+                "EhExPee": 0,
+                "Seesaw": 0,
+                "Twosoul": 0
+            },
+            "constellations": {
+
+            },
+            "starSigns": {
+
+            }
+        });
+    }
+    return r;
+}
+
+function parseCompanionStatues(clean) {
+
 }
