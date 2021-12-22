d4b311724caf95227a5b20b5e2aa7b1d195f3c49
Corbeno <corbin@kemsfamily.us>
2021-12-22 14:54:40 -0600

    clean up code and add new data gathering

--- js/injected.js
+++ js/injected.js
@@ -4,45 +4,57 @@ async function queryFirebaseWithRetry() {
     let count = 0;
     let maxTimeout = 20;
     while (true) {
-        await sleep(1000);
-        try {
-            let usernameList = Q.getUserNameList();
-            if (usernameList == null) {
-                count++;
-                continue;
-            }
-
-            //TESTING
-            // r.child(Ge).once("value", function(dataSnapshot){
-            //     try{
-            //         console.log(dataSnapshot.exportVal());
-            //     }catch(e){
-            //         //ignore
-            //     }
-            // });
-
-            var send = new CustomEvent("PassCharNameToInject", { detail: usernameList });
-            window.dispatchEvent(send);
-
-            //doc(l) is a variable that holds the user id of the logged in user for firebase
-            d.collection("_data").doc(l).get().then(doc => {
-                //send save data to extension
-                var event = new CustomEvent("PassSaveToInject", { detail: doc.data() });
-                window.dispatchEvent(event);
-            });
-
-            //TODO ADD GUILD DATA
-            var send = new CustomEvent("PassGuildInfoToInject", { detail: {} });
-            window.dispatchEvent(send);
-
+        if(count > maxTimeout){
+            console.error("Reached max timeout to check variables. Did their names change?");
             break;
-        } catch (e) {
-            if (count > maxTimeout) {
-                console.e("Max timeout of " + maxTimeout + " reached for getting firebase object! Please retry. If loading idleon takes longer then this time, ")
+        }
+        await sleep(1000);
+
+        //grab globally accessable variables from other scripts (mainly Z.js and firebase.js)
+        //it is configured this way to be easy to change when the minified variable names possibly change
+        let external = {
+            usernameList: v,
+            database: d,
+            userId: l,
+            guildId: ae
+        }
+        //verify all variables have been obtained
+        let isBadData = false;
+        for(const key in external){
+            if(external[key] == null || external[key] == undefined){
+                isBadData = true;
                 break;
             }
-            count++;
         }
+        if(isBadData){
+            count++;
+            continue;
+        };
+
+        var send = new CustomEvent("PassCharNameToInject", { detail: external.usernameList });
+        window.dispatchEvent(send);
+
+        external.database.collection("_data").doc(external.userId).get().then(doc => {
+            var event = new CustomEvent("PassSaveToInject", { detail: doc.data() });
+            window.dispatchEvent(event);
+        });
+
+        external.database.collection("_guildStat").doc(external.guildId).get().then(doc => {
+            var send = new CustomEvent("PassGuildInfoToInject", { detail: doc.data() });
+            window.dispatchEvent(send);
+        });
+
+        //TESTING - This is a lot of data so it shouldn't be grabbed every time...
+        //maybe make this data be grabbed when a button is clicked?
+        // r.child(Ge).once("value", function(dataSnapshot){
+        //     try{
+        //         console.log(dataSnapshot.exportVal()[0]);
+        //     }catch(e){
+        //         //ignore
+        //     }
+        // });
+
+        break;
     }
 }
 
