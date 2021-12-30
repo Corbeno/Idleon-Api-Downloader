8e770476d2f79871be6b92a2b5c64ab9ce617481
Corbeno <corbin@kemsfamily.us>
2021-12-29 21:58:27 -0600

    initial work.. really messy lol

--- js/popup.js
+++ js/popup.js
@@ -1,3 +1,5 @@
+openTab(event, "tab1");
+
 updateAllButtons();
 chrome.storage.onChanged.addListener(function (changes, namespace) {
     for (let [key, {
@@ -140,4 +142,22 @@ function showTooltip(e, text) {
     setTimeout(() => {
         tooltip.style.display = 'none';
     }, 1000)
+}
+
+function openTab(evt, tabName) {
+    // Get all elements with class="tabcontent" and hide them
+    let tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    let tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
 }
